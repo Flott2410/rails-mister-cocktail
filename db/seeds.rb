69957223ef27252da29e440527fb34b6e2944c4a
Florian Ott <florian.ott@gmx.at>
2020-08-20 12:25:13 +0100

    models created and validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Create ingredients"
+filepath = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized_ingredients = File.read(filepath)    # File.read Method - reads the file
+ingredients = JSON.parse(serialized_beers)
+ingredients_array = ingredients[:drinks]
+ingredients_array.each do |ingredient|
+  Ingredient.create!(name: ingredient.value)
+end
+
+puts "Successfully created ingredients"
